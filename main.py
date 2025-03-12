a372734e14977d681e15eee0e89aa001ff95b7ba
Student Opencode <student.opencode24@gmail.com>
2025-03-12 10:15:42 +0700

    переезд

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def one1():
     back_m.run_angle(200, 95) #запуск мотора для сбора ящиков
     front_m.run(-9999)
     move_speed_change(240, 400) #подъезд к теплице. Блок 1
-    wait(3) # ожидание пока собирает ящики
+    wait(200) # ожидание пока собирает ящики
     move_speed_change(-50, 200)
     move_speed_change(100, 400) #подъезд к теплице. Блок 2
-    wait(3) # ожидание пока собирает ящики
+    wait(200) # ожидание пока собирает ящики
     move_speed_change(-150,100)
     motor.straight(-410)
     front_m.stop()
@@ -56,28 +56,28 @@ def three():
 def four1():
     """переезд в красеую зону, с картошкой и зернохранилищем
     """
-    back_m.run_angle(400, 100, wait= 0)
-    motor.straight(-40)
-    motor.straight(350)
-    turn_acc_change(85, rate=1000)
+    front_m.run_angle(800, -7, wait=0)
+    motor.straight(-10)
+    motor.straight(355)
+    turn_acc_change(90, rate=1000)
     for i in range(1):
         motor.straight(273)
         motor.straight(-70)
     motor.straight(-70)
     motor.turn(-100)
     motor.straight(620)
-    motor.turn(102)
-    motor.straight(-470)
-    motor.drive(-100, 0) #подъезд к картофелю
-    for _ in range(2): #несколько подъездов к кортофелю
-        motor.straight(-150)
-        motor.straight(60)
-    motor.straight(130) # отъезд от кортофли
-    motor.turn(-91) #сбор винограда
-    back_m.run_angle(300, 90)
-    move_speed_change(400, 200)
-    back_m.run_angle(300, -80)
-    move_speed_change(900, 400)
+    motor.turn(104)
+    front_m.run_angle(800, 50, wait=0)
+    move_speed_change(-550, 400) #подъезд к картофелю
+    motor.straight(60)
+    motor.straight(-150)
+    motor.straight(138) # подъезд к комбайну
+    back_m.run_angle(800, -1560)
+    back_m.run_angle(800, 1580, wait=0)
+    wait(500)
+    motor.straight(60) 
+    motor.turn(-89) #сбор винограда
+    motor.straight(1100)
 
 def four2():
     """переезд в красную зону, с картошкой без зернохранилища 
